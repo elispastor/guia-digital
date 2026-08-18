03a516b4d59bd88fc87f1fba607d9de926319941
Elis Suárez <elispastorst@gmail.com>
2026-08-18 13:46:05 -0400

    Uncomment RED.start() to start the Node-RED server

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const RED = require('node-red');
 const express = require('express');
 const app = express();
-
 const server = app.listen(process.env.PORT || 1880, () => {
     console.log('Servidor SAI corriendo en puerto', server.address().port);
 });
-
 RED.init(server, {
     httpAdminRoot: '/',
     httpNodeRoot: '/api',
     userDir: __dirname,
     flowFile: 'flows.json'
 });
-
 RED.start();
